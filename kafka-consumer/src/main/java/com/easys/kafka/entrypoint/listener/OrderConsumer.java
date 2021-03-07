14a705b3c5f22affe71ec79dadfad72ac77840a1
pudimm <40968645+pudimm@users.noreply.github.com>
2021-03-07 19:15:33 -0300

    colocando mais informacoes sobre a mensagem no consumidor

--- kafka-consumer/src/main/java/com/easys/kafka/entrypoint/listener/OrderConsumer.java
+++ kafka-consumer/src/main/java/com/easys/kafka/entrypoint/listener/OrderConsumer.java
@@ -1,5 +1,6 @@
 package com.easys.kafka.entrypoint.listener;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,8 +11,12 @@ public class OrderConsumer {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
 
+	@SuppressWarnings("rawtypes")
 	@KafkaListener(topics = "${order.topic}", groupId = "${spring.kafka.consumer.group-id}")
-	public void consumer(String order) {
-		LOGGER.info(order);
+	public void consumer(final ConsumerRecord consumerRecord) {
+		LOGGER.info("Mensagem: " + consumerRecord.value());
+		LOGGER.info("Particao: " + consumerRecord.partition());
+		LOGGER.info("Data: " + consumerRecord.timestamp());
+		LOGGER.info("Topico: " + consumerRecord.topic());
 	}
 }
